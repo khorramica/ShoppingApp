bf57c97721853733496f11a6ea33cd013faa3d53
Reza kh <khorami.reza55@gmail.com>
2022-06-03 14:24:18 +0430

    Initial commit

---
+++ app/src/main/java/ir/finto/shoppingapp/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package ir.finto.shoppingapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.finto.shoppingapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
